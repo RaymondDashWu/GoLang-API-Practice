4b05b32423a17b0a92d748f08df0613281523eb0
RaymondDashWu <raymond31670@hotmail.com>
2019-02-12 11:38:46 -0800

    [ADD] Fetching from Ron Swanson API

--- API_Practice.go
+++ API_Practice.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"log"
+	"encoding/json"
 	"net/http"
+	"time"
 
 	"github.com/labstack/echo"
 )
@@ -10,15 +11,41 @@ import (
 // API to use
 // http://ron-swanson-quotes.herokuapp.com/v2/quotes
 
+// Reference
+// https://stackoverflow.com/questions/17156371/how-to-get-json-response-in-golang
+
+var myClient = &http.Client{Timeout: 10 * time.Second}
+
+func getJSON(url string, target *swansonQuote) error {
+	resp, err := myClient.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	res := []string{}
+	json.NewDecoder(resp.Body).Decode(&res)
+	target.Quote = res[0]
+	return nil
+}
+
+type swansonQuote struct {
+	Quote string `json:"quote"`
+}
+
 func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
-		resp, err := http.Get("http://ron-swanson-quotes.herokuapp.com/v2/quotes")
+		// resp, err := http.Get("http://ron-swanson-quotes.herokuapp.com/v2/quotes")
 
-		if err != nil {
-			log.Fatal("Request could not be completed")
-		}
-		// return c.String(http.StatusOK, "Hello, World!")
+		// if err != nil {
+		// 	log.Fatal("Request could not be completed")
+		// }
+		// return c.String(http.StatusOK, resp)
+		testQuote := swansonQuote{}
+		getJSON("http://ron-swanson-quotes.herokuapp.com/v2/quotes", &testQuote)
+		return c.JSON(http.StatusOK, testQuote)
+
 	})
 	e.Logger.Fatal(e.Start(":1323"))
 }
